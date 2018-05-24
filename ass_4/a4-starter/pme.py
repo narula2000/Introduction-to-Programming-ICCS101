e38f90c42de509d7f5c7dbf6849a7993e20d8237
Vikrom Narula <djnarula@gmail.com>
2018-05-24 11:11:05 +0700

    works pme

--- ass_4/a4-starter/pme.py
+++ ass_4/a4-starter/pme.py
@@ -1,12 +1,31 @@
+def checkprime(n):
+    if n<2: return False
+    else:
+     divisible = False
+     for d in range(2, n):
+        if n % d == 0:
+           divisible = True
+           break
+     # prime if none of these divide x
+     return not divisible
+
+def plus_unit(a):
+    acc=0
+    s=str(a)
+    for b in range(1,len(s)+1):
+        acc=acc+int(s[-b])
+    return acc
+
+
 def primeMeatExtract(x):
     """
 
-    >>> primeMeatExtract(1)
-    None
+    >>> primeMeatExtract(1) 
+    True
     >>> primeMeatExtract(3)
     3
-    >>> primeMeatExtract(4)
-    None
+    >>> primeMeatExtract(4) 
+    True
     >>> primeMeatExtract(11)
     11
     >>> primeMeatExtract(642)
@@ -14,10 +33,27 @@ def primeMeatExtract(x):
     >>> primeMeatExtract(128)
     11
     >>> primeMeatExtract(886)
-    None
+    True
 
     """
-    return ___
+    if x<=1:
+        return True
+    if x<=9 and x>1:
+        if checkprime(x)== True:
+            return x
+        else:
+            return True
+
+    while checkprime(x)!=True and x>9:
+        x=plus_unit(x)
+    if x<=9 and x>1:
+        if checkprime(x)== True:
+            return x
+        else:
+            return True
+    if checkprime(x)== True:
+        return x
+
 
 
 ###########################################################################
@@ -28,5 +64,5 @@ def primeMeatExtract(x):
 ###########################################################################
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    doctest.testmod(verbose=True)
 ###########################################################################
