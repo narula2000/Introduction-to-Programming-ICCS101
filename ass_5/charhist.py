4baac82595de4d360efd340fdf7b7b630fe22025
Vikrom Narula <djnarula@gmail.com>
2018-06-05 17:23:25 +0700

    first question ass5

---
+++ ass_5/charhist.py
@@ -0,0 +1,27 @@
+def charHistogram(filename):
+    text= open(filename, 'r')
+    line= text.readlines()
+    table=dict()
+    alp='abcdefghijklmnopqrstuvwxyz'
+    for w in line:
+        w_n= w.strip()
+        for s in range(0,len(w_n)):
+            for h in w[s]:
+                l= h.lower()
+                
+                if l in table:
+                    table[l]+=1
+                else:
+                    table[l]=1
+#    for let in table.keys():
+#        for al in range(0,len(alp)):
+#            if let==alp[al]:
+#                print(let, (table[let]*'+'))
+    for al in range(0,len(alp)):
+        for let in table.keys():
+            if alp[al]==let:
+                print(let, (table[let]*'+'))
+    text.close()
+print(charHistogram("C:\\Users\\asus\\Dropbox\\MUIC\\Intro Pro\\ass_5\\test5.txt"))
+
+
