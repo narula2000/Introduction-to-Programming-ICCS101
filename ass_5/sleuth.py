5962484f39d38d0c6a40ebff52bafef249248ddf
Vikrom Narula <djnarula@gmail.com>
2018-06-13 17:22:09 +0700

    fucking hell

---
+++ ass_5/sleuth.py
@@ -0,0 +1,67 @@
+def stringmak(grid):
+    h=''
+    for li in grid:
+        for w in li:
+            h+=w
+    return h
+
+
+def containsWord(grid, w):
+    lsring=stringmak(grid)
+    l=[]
+    gap=len(grid[0])
+    cou=0
+    for word in w:
+        if word in lsring:
+            l.append(word)
+        if word[0] in lsring:  #check vertical
+            for b in range(len(lsring)):
+                if word[0]==lsring[b]:
+                    e=b
+                    for letter in range(len(word)):
+                        
+                        if len(lsring)>=e+(gap*letter) and word[letter]==lsring[e+(gap*letter)]:
+                            cou+=1
+                        else:
+                            cou=0
+                    if cou==len(word):
+                        l.append(word)
+                        cou=0
+        if word[0] in lsring:  
+                for b in range(len(lsring)):
+                    if word[0]==lsring[b]:
+                        e=b
+                        for letter in range(len(word)):
+                            
+                            if len(lsring)>e+(gap*letter)+letter and word[letter]==lsring[e+(gap*letter)+letter] :
+                                cou+=1
+                            else:
+                                cou=0
+                        if cou==len(word):
+                            l.append(word)
+                            cou=0
+        if word[0] in lsring:  
+                for b in range(len(lsring)):
+                    if word[0]==lsring[b]:
+                        e=b
+                        for letter in range(len(word)):
+                            
+                            if len(lsring)>e+(gap*letter)+letter and word[letter]==lsring[e+(gap*letter)-letter] :
+                                cou+=1
+                            else:
+                                cou=0
+                        if cou==len(word):
+                            l.append(word)
+                            cou=0
+    return l&l
+
+
+print(containsWord([["r","a","w","b","i","t"],
+["x","a","y","z","c","h"],
+["p","q","b","e","i","e"],
+["t","r","s","b","o","g"],
+["u","w","x","v","i","t"],
+["n","m","r","w","o","t"]],["bog", "moon", "rabbit", "the", "bit",
+"raw"]))
+
+
