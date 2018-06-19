4332d44c2df6e6fa6819f8f1cec71c00ab5fb50c
Vikrom Narula <djnarula@gmail.com>
2018-06-19 18:26:57 +0700

    finishes with 2,3

---
+++ ass_6/a6-starter/eto.py
@@ -0,0 +1,17 @@
+# Assignment 06, Task 03
+# Name:Vikrom Narula
+# Time: 80 min 
+
+def eto(lst):
+    if len(lst)==1:
+        return lst
+    else:
+        x=eto(lst[1:])
+        if lst[0]%2==0:
+            return [lst[0]]+x
+        else:
+            return x+[lst[0]]
+
+
+
+
--- ass_6/a6-starter/findme.py
+++ ass_6/a6-starter/findme.py
@@ -1,6 +1,6 @@
 # Assignment 06, Task 02
 # Name:Vikrom Narula
-# Time: 5 min 
+# Time: 30 min 
 
 
 
@@ -8,4 +8,17 @@
 
 
 def findMe(elt, lst):
-    
+    if elt not in lst:
+        return None
+    if lst[0]==elt:
+        return 0
+    else:
+        n_lst=lst[1:]
+        x=findMe(elt,n_lst)
+        return x+1
+
+assert(findMe(9, [1,2,9,5,3,9])==2)
+assert(findMe(7, [1,2,3,4,9])==None)
+assert(findMe('nag',[1,2,3,'nag',4])==3)
+
+print('Pass')
