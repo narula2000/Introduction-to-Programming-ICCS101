1a893165ae74899623262fe8dabf68aff2d9fb77
Vikrom Narula <djnarula@gmail.com>
2018-06-26 19:04:26 +0700

    done left task 4

--- ass_6/a6-starter/a6-starter/run_2048.py
+++ ass_6/a6-starter/a6-starter/run_2048.py
@@ -5,7 +5,7 @@
 from tkinter import Tk, Canvas
 import random
 from tkinter import messagebox as tkMessageBox
-import sol_t2048 as t2048
+import t2048 as t2048
 
 class Tile(Canvas):
     """ UI for each tile """
--- ass_6/a6-starter/a6-starter/t2048.py
+++ ass_6/a6-starter/a6-starter/t2048.py
@@ -1,7 +1,7 @@
-# Assignment XX, Task YY
-# Name: Eye Loveprograming
-# Collaborators: John Nonexistent
-# Time Spent: 4:00 hrs
+# Assignment 06, Task 05
+# Name: Vikrom Narula
+# Collaborators: 1409
+# Time Spent: 8:00 hrs
 # ----------------------------------
 
 
@@ -9,7 +9,19 @@
 # possible move left. If no more moves,  
 # return True. Otherwise return False.
 def isGameOver(board):
-    return False
+    for r in range(len(board)):
+        for c in range(len(board[0])-1):
+            if board[r][c+1]==' ' or board[r][c]==' ':
+                return False
+            elif board[r][c]==board[r][c+1]:
+                return False
+    for r in range(len(board)-1):
+        for c in range(len(board[0])):
+            if board[r+1][c]==' ' or board[r+1][c]==' ':
+                return False
+            elif board[r][c]==board[r+1][c]:
+                return False
+    return True
 
 # Returns a tuple (changed, new_board) 
 # where:
@@ -18,7 +30,26 @@ def isGameOver(board):
 #  new_board - the board after the user
 #              presses the 'Up' key.
 def doKeyUp(board):
-    return False, board
+    checker=[]
+    for cop in board:
+        checker.append(cop[:])
+    for times in range(len(checker)): #shif all up
+        for r in range(len(checker)-1):
+            for c in range(len(checker[0])):
+                if checker[r][c]==' ':
+                    checker[r][c]=checker[r+1][c]
+                    checker[r+1][c]=' '
+    for r in range(len(checker)-1): # Add same num
+        for c in range(len(checker[0])):
+            if checker[r][c]==checker[r+1][c] and checker[r][c]!=' ':
+                checker[r][c]=str(int(checker[r][c])*2)
+                checker[r+1][c]=' '        
+    for r in range(len(checker)-1):  #Shift up once
+        for c in range(len(checker[0])): 
+            if checker[r][c]==' ':
+                checker[r][c]=checker[r+1][c]
+                checker[r+1][c]=' '
+    return ((board!=checker),checker)
 
 # Returns a tuple (changed, new_board) 
 # where:
@@ -27,7 +58,26 @@ def doKeyUp(board):
 #  new_board - the board after the user
 #              presses the 'Down' key.
 def doKeyDown(board):
-    return False, board
+    checker=[]
+    for cop in board:
+        checker.append(cop[:])
+    for times in range(len(checker)): #shif all up
+        for r in range(len(checker)-1,0,-1):
+            for c in range(len(checker[0])):
+                if checker[r][c]==' ':
+                    checker[r][c]=checker[r-1][c]
+                    checker[r-1][c]=' '
+    for r in range(len(checker)-1,0,-1): # Add same num
+        for c in range(len(checker[0])):
+            if checker[r][c]==checker[r-1][c] and checker[r][c]!=' ':
+                checker[r][c]=str(int(checker[r][c])*2)
+                checker[r-1][c]=' '        
+    for r in range(len(checker)-1,0,-1):  #Shift up once
+        for c in range(len(checker[0])): 
+            if checker[r][c]==' ':
+                checker[r][c]=checker[r-1][c]
+                checker[r-1][c]=' '
+    return ((board!=checker),checker)
 
 # Returns a tuple (changed, new_board) 
 # where:
@@ -36,7 +86,26 @@ def doKeyDown(board):
 #  new_board - the board after the user
 #              presses the 'Left' key.
 def doKeyLeft(board):
-    return False, board
+    checker=[]
+    for cop in board:
+        checker.append(cop[:])
+    for times in range(len(checker[0])): #shif all up
+        for r in range(len(checker)):
+            for c in range(len(checker[0])-1):
+                if checker[r][c]==' ':
+                    checker[r][c]=checker[r][c+1]
+                    checker[r][c+1]=' '
+    for r in range(len(checker)): # Add same num
+        for c in range(len(checker[0])-1):
+            if checker[r][c]==checker[r][c+1] and checker[r][c]!=' ':
+                checker[r][c]=str(int(checker[r][c])*2)
+                checker[r][c+1]=' '        
+    for r in range(len(checker)):  #Shift up once
+        for c in range(len(checker[0])-1): 
+            if checker[r][c]==' ':
+                checker[r][c]=checker[r][c+1]
+                checker[r][c+1]=' '
+    return ((board!=checker),checker)
 
 
 # Returns a tuple (changed, new_board) 
@@ -46,17 +115,49 @@ def doKeyLeft(board):
 #  new_board - the board after the user
 #              presses the 'Right' key.
 def doKeyRight(board):
-    return False, board
+    checker=[]
+    for cop in board:
+        checker.append(cop[:])
+    for times in range(len(checker[0])): #shif all up
+        for r in range(len(checker)):
+            for c in range(len(checker[0])-1,0,-1):
+                if checker[r][c]==' ':
+                    checker[r][c]=checker[r][c-1]
+                    checker[r][c-1]=' '
+    for r in range(len(checker)): # Add same num
+        for c in range(len(checker[0])-1,0,-1):
+            if checker[r][c]==checker[r][c-1] and checker[r][c]!=' ':
+                checker[r][c]=str(int(checker[r][c])*2)
+                checker[r][c-1]=' '        
+    for r in range(len(checker)):  #Shift up once
+        for c in range(len(checker[0])-1,0,-1): 
+            if checker[r][c]==' ':
+                checker[r][c]=checker[r][c-1]
+                checker[r][c-1]=' '
+    return ((board!=checker),checker)
 
 
 # Returns a list of tuples (row, col)
 # indicating where the empty spots are
 def emptyPos(board):
-    return []
+    lst=[]
+    for r in range(len(board)):
+        for c in range(len(board[0])):
+            if board[r][c]==' ':
+                lst.append((r, c))
+    return lst
 
 
 # Returns a dictionary mapping each tile 
 # value on the board to its count (i.e.,
 # how many times it appears on the board)
 def hist(board):
-    return dict()
+    di=dict()
+    for r in range(len(board)):
+        for c in range(len(board[0])):
+            if board[r][c]!=' ':
+                if board[r][c] not in di:
+                    di[board[r][c]]=1
+                else:
+                    di[board[r][c]]=board[r][c]+1
+    return di
