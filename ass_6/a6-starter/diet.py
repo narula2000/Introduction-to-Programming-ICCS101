4f7228745b8812fab7bda10eb570f624470fd477
Vikrom Narula <djnarula@gmail.com>
2018-06-27 19:24:31 +0700

    Fucking done left assert

--- ass_6/a6-starter/diet.py
+++ ass_6/a6-starter/diet.py
@@ -1,8 +1,33 @@
+# Assignment 06, Task 0
+# Name:Vikrom Narula
+# Time:  45:00 min
+
+def t_cal(db):
+    d=dict()
+    for ing in db:
+        name, cal = ing.split(':')
+        Cr, Pr, Ft= cal.split(',')
+        d[name]=(float(Cr)*4)+(float(Pr)*4)+(float(Ft)*9)
+    return d
+
 def mealCal(meal, recipes, db):
-    for m_meal in meal:
-        for e_recipes in recipes:
-            if m_meal in e_recipes:
-                m, ingre= e_recipes.split(':')
-                for cal in bd:
-                    ingre1,calie= cal.split(':')
-                    if ingre1 in ingre:   
+    tcal=t_cal(db)
+    cal=0
+    for food in meal:
+        for how in recipes:
+            if food in how:
+                name, ing= how.split(':')
+                for way in ing.split(','):
+                    ing1, quan= way.split('*')
+                    cal=cal+tcal[ing1]*float(quan)
+    return cal
+
+
+recipes = ["Pork Stew:Cabbage*5,Carrot*1,Fatty Pork*10",
+"Green Salad1:Cabbage*10,Carrot*2,Pineapple*5",
+"T-Bone:Carrot*2,Steak Meat*1"]
+meal = ["T-Bone", "T-Bone", "Green Salad1"]
+db = ["Cabbage:4,2,0", "Carrot:9,1,5", "Fatty Pork:431,1,5",
+"Pineapple:7,1,0", "Steak Meat:5,20,10", "Rabbit Meat:7,2,20"]
+print(mealCal(meal, recipes, db))
+
