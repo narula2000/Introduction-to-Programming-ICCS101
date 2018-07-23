acc665e2b14067b103d37390590f988ed60e4b10
Vikrom Narula <djnarula@gmail.com>
2018-07-23 21:00:54 +0700

    done

--- ass_5/sleuth.py
+++ ass_5/sleuth.py
@@ -1,69 +1,64 @@
-#Assignment 05, Task 03 
-#Name: Vikrom Narula
+# Assignment 05, Task 03
+# Name: Vikrom Narula
 
-#Time Spent: 13 hrs
+# Time Spent: 13 hrs
 
 
 def stringmak(grid):
-    h=''
+    h = ''
     for li in grid:
         for w in li:
-            h+=w
+            h += w
     return h
 
 
 def wordSleuth(w, grid):
-    lsring=stringmak(grid)
-    l=[]
-    gap=len(grid[0])
-    cou=0
+    lsring = stringmak(grid)
+    l = []
+    gap = len(grid[0])
+    cou = 0
     for word in w:
         for longie in grid:
             if word in stringmak(longie):
                 l.append(word)
-        if word[0] in lsring:  #check vertical
+        if word[0] in lsring:  # check vertical
             for b in range(len(lsring)):
-                if word[0]==lsring[b]:
-                    e=b
+                if word[0] == lsring[b]:
+                    e = b
                     for letter in range(len(word)):
-                        
-                        if len(lsring)>=e+(gap*letter) and word[letter]==lsring[e+(gap*letter)]:
-                            cou+=1
+
+                        if len(lsring) >= e+(gap*letter) and word[letter] == lsring[e+(gap*letter)]:
+                            cou += 1
                         else:
-                            cou=0
-                    if cou==len(word):
+                            cou = 0
+                    if cou == len(word):
                         l.append(word)
-                        cou=0
-        if word[0] in lsring:  #check righ down
-                for b in range(len(lsring)):
-                    if word[0]==lsring[b]:
-                        e=b
-                        for letter in range(len(word)):
-                            
-                            if len(lsring)>e+(gap*letter)+letter and word[letter]==lsring[e+(gap*letter)+letter] :
-                                cou+=1
-                            else:
-                                cou=0
-                        if cou==len(word):
-                            l.append(word)
-                            cou=0
-                            
-        if word[0] in lsring:  #check left up
-                for b in range(len(lsring)):
-                    if word[0]==lsring[b]:
-                        e=b
-                        for letter in range(len(word)):
-                            
-                            if len(lsring)>e+(gap*letter)+letter and word[letter]==lsring[e-(gap*letter)+letter] :
-                                cou+=1
-                            else:
-                                cou=0
-                        if cou==len(word):
-                            l.append(word)
-                            cou=0
+                        cou = 0
+        if word[0] in lsring:  # check righ down
+            for b in range(len(lsring)):
+                if word[0] == lsring[b]:
+                    e = b
+                    for letter in range(len(word)):
+
+                        if len(lsring) > e+(gap*letter)+letter and word[letter] == lsring[e+(gap*letter)+letter]:
+                            cou += 1
+                        else:
+                            cou = 0
+                    if cou == len(word):
+                        l.append(word)
+                        cou = 0
+
+        if word[0] in lsring:  # check left up
+            for b in range(len(lsring)):
+                if word[0] == lsring[b]:
+                    e = b
+                    for letter in range(len(word)):
+
+                        if len(lsring) > e+(gap*letter)+letter and word[letter] == lsring[e-(gap*letter)+letter]:
+                            cou += 1
+                        else:
+                            cou = 0
+                    if cou == len(word):
+                        l.append(word)
+                        cou = 0
     return list(set(l))
-
-
-
-
-
