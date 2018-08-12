2102f9d531d24fcacca2c6b47ed783d164b659ef
Vikrom Narula <djnarula@gmail.com>
2018-08-12 13:57:34 +0700

    gg

--- ass_5/charhist.py
+++ ass_5/charhist.py
@@ -1,32 +1,30 @@
-#Assignment 05, Task 01 
-#Name: Vikrom Narula
+# Assignment 05, Task 01
+# Name: Vikrom Narula
 
-#Time Spent: 1 hrs
+# Time Spent: 1 hrs
+
 
 def charHistogram(filename):
-    text= open(filename, 'r')
-    line= text.readlines()
-    table=dict()
-    alp='abcdefghijklmnopqrstuvwxyz'
+    text = open(filename, 'r')
+    line = text.readlines()
+    table = dict()
+    alp = 'abcdefghijklmnopqrstuvwxyz'
     for w in line:
-        w_n= w.strip()
-        for s in range(0,len(w_n)):
+        w_n = w.strip()
+        for s in range(0, len(w_n)):
             for h in w[s]:
-                l= h.lower()
-                
+                l = h.lower()
+
                 if l in table:
-                    table[l]+=1
+                    table[l] += 1
                 else:
-                    table[l]=1
+                    table[l] = 1
 #    for let in table.keys():
 #        for al in range(0,len(alp)):
 #            if let==alp[al]:
 #                print(let, (table[let]*'+'))
-    for al in range(0,len(alp)):
+    for al in range(0, len(alp)):
         for let in table.keys():
-            if alp[al]==let:
+            if alp[al] == let:
                 print(let, (table[let]*'+'))
     text.close()
-
-
-
