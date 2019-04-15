5c1ada30bf2812cd93897b4636410e2a9502ab06
Vikrom Narula <djnarula@gmail.com>
2019-04-15 15:09:33 +0700

    format

--- ass_6/a6-starter/findme.py
+++ ass_6/a6-starter/findme.py
@@ -1,21 +1,14 @@
 # Assignment 06, Task 02
 # Name:Vikrom Narula
-# Time: 30 min 
-
-
-
-
+# Time: 30 min
 
 
 def findMe(elt, lst):
     if elt not in lst:
         return None
-    if lst[0]==elt:
+    if lst[0] == elt:
         return 0
     else:
-        n_lst=lst[1:]
-        x=findMe(elt,n_lst)
+        n_lst = lst[1:]
+        x = findMe(elt, n_lst)
         return x+1
-
-
-
