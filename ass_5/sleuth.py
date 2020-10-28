c54cfe94688eca84a7570049477bbc0d91a3f66a
Vikrom Narula <djnarula@gmail.com>
2020-10-28 17:42:21 +0700

    Refactored

--- ass_5/sleuth.py
+++ ass_5/sleuth.py
@@ -2,62 +2,76 @@
 # Name: Vikrom Narula
 # Time Spent: 13 hrs
 
+def gridStringMaker(grid):
+    buffer = ''
+    for lst in grid:
+        for char in lst:
+            buffer += char
+    return buffer
 
-def stringmak(grid):
-    h = ''
-    for li in grid:
-        for w in li:
-            h += w
-    return h
+
+def stringMaker(lst):
+    buffer = ''
+    for char in lst:
+        buffer += char
+    return buffer
 
 
-def wordSleuth(w, grid):
-    lsring = stringmak(grid)
-    l = []
-    gap = len(grid[0])
-    cou = 0
-    for word in w:
-        for longie in grid:
-            if word in stringmak(longie):
-                l.append(word)
-        if word[0] in lsring:  # check vertical
-            for b in range(len(lsring)):
-                if word[0] == lsring[b]:
-                    e = b
-                    for letter in range(len(word)):
+def checkVertical(word, string, char, size):
+    counter = 0
+    for letter in range(len(word)):
+        if len(string) > char + (size * letter) and word[letter] == string[char + (size * letter)]:
+            counter += 1
+        else:
+            break
+    return counter
 
-                        if len(lsring) >= e+(gap*letter) and word[letter] == lsring[e+(gap*letter)]:
-                            cou += 1
-                        else:
-                            cou = 0
-                    if cou == len(word):
-                        l.append(word)
-                        cou = 0
-        if word[0] in lsring:  # check righ down
-            for b in range(len(lsring)):
-                if word[0] == lsring[b]:
-                    e = b
-                    for letter in range(len(word)):
 
-                        if len(lsring) > e+(gap*letter)+letter and word[letter] == lsring[e+(gap*letter)+letter]:
-                            cou += 1
-                        else:
-                            cou = 0
-                    if cou == len(word):
-                        l.append(word)
-                        cou = 0
+def checkRightDown(word, string, char, size):
+    counter = 0
+    for letter in range(len(word)):
+        if len(string) > char + (size * letter) + letter and word[letter] == string[char + (size * letter) + letter]:
+            counter += 1
+        else:
+            break
+    return counter
 
-        if word[0] in lsring:  # check left up
-            for b in range(len(lsring)):
-                if word[0] == lsring[b]:
-                    e = b
-                    for letter in range(len(word)):
 
-                        if len(lsring) > e+(gap*letter)+letter and word[letter] == lsring[e-(gap*letter)+letter]:
-                            cou += 1
-                        else:
-                            cou = 0
-                    if cou == len(word):
-                        l.append(word)
-                        cou = 0
-    return list(set(l))
+def checkLeftDown(word, string, char, size):
+    counter = 0
+    for letter in range(len(word)):
+        if len(string) > char + (size * letter) + letter and word[letter] == string[char - (size * letter) + letter]:
+            counter += 1
+        else:
+            break
+    return counter
+
+
+def wordSleuth(grid, words):
+    string = gridStringMaker(grid)
+    lst = []
+    size = len(grid[0])
+    for word in words:
+        for lst in grid:
+            if word in stringMaker(lst):
+                lst.append(word)
+
+        if word[0] in string:
+            for char in range(len(string)):
+                if word[0] == string[char]:
+                    if checkVertical(word, string, char, size) == len(word):
+                        lst.append(word)
+                    if checkRightDown(word, string, char, size) == len(word):
+                        lst.append(word)
+                    if checkLeftDown(word, string, char, size) == len(word):
+                        lst.append(word)
+
+    return list(set(lst))
+
+
+assert(wordSleuth(
+    [["r", "a", "w", "b", "i", "t"], ["x", "a", "y", "z", "c", "h"],
+     ["p", "q", "b", "e", "i", "e"], ["t", "r", "s", "b", "o", "g"],
+     ["u", "w", "x", "v", "i", "t"], ["n", "m", "r", "w", "o", "t"]],
+    ["raw", "bit",   "rabbit", "bog", "the"]
+))
